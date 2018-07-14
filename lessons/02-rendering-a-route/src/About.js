91c7c11e7b8d394a81430e8d477320157720babd
Gordon Yang <xiaya@microsoft.com>
2018-07-14 23:08:22 +0000

    add basic route rendering operation

---
+++ lessons/02-rendering-a-route/src/About.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>This is About page!</div>
+        );
+    }
+}
+
+export default About;
--- lessons/02-rendering-a-route/src/index.js
+++ lessons/02-rendering-a-route/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route} from 'react-router-dom';
+import About from './About';
+import Repos from './Repos';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={App}/>
+            <Route path="/repos" component={Repos}/>
+            <Route path="/about" component={About}/>
+        </div>
+    </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
