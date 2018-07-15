671293b1bd490051f61ac5f49fb5924ab9eef683
Gordon Yang <xiaya@microsoft.com>
2018-07-15 06:17:03 +0000

    add link

--- lessons/03-navigating-with-link/src/App.js
+++ lessons/03-navigating-with-link/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-        <div>This is our first react-router V4 tutorial</div>
+        <div>
+            <h1>This is our first react-router V4 tutorial</h1>
+            <ul role="nav">
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/repos">Repos</Link></li>
+            </ul>
+        </div>
     );
   }
 }
