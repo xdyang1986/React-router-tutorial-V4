63ec11b3b267a439da0a3808f7aa650400c1c5ac
Gordon Yang <xiaya@microsoft.com>
2018-07-15 06:05:36 +0000

    add 02 lesson to be the basic of 03

---
+++ lessons/03-navigating-with-link/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>This is our first react-router V4 tutorial</div>
+    );
+  }
+}
+
+export default App;
---
+++ lessons/03-navigating-with-link/src/Repos.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Repos extends Component {
+    render() {
+        return (
+            <div>This is Repos page!</div>
+        );
+    }
+}
+
+export default Repos;
