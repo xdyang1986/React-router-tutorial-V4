9ea9f7a6b791729d0d2516909d099ee91664385b
Gordon Yang <xiaya@microsoft.com>
2018-07-15 10:05:43 +0000

    add exact for /

--- lessons/02-rendering-a-route/src/index.js
+++ lessons/02-rendering-a-route/src/index.js
@@ -10,7 +10,7 @@ import Repos from './Repos';
 ReactDOM.render((
     <BrowserRouter>
         <div>
-            <Route path="/" component={App}/>
+            <Route exact path="/" component={App}/>
             <Route path="/repos" component={Repos}/>
             <Route path="/about" component={About}/>
         </div>
